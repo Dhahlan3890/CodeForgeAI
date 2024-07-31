95b9c5bf5b5d76b2166141dba0d0d555226c526b
Dhahlan3890 <dhahlan2000@gmail.com>
2024-07-31 22:39:31 +0530

    modified for production deploy

--- code_backend/code_backend/urls.py
+++ code_backend/code_backend/urls.py
@@ -20,10 +20,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('myapp.urls')),
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += staticfiles_urlpatterns()
 
