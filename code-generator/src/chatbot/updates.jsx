e5871a28dbb0065691ffd9c16986b28be9bc46f0
Dhahlan3890 <dhahlan2000@gmail.com>
2025-03-02 14:59:15 +0530

    Update chatbot updates to include initial release message and add advanced/dark mode features

--- code-generator/src/chatbot/updates.jsx
+++ code-generator/src/chatbot/updates.jsx
@@ -25,8 +25,10 @@ import {
             </TimelineHeader>
             <TimelineBody className="pb-8">
               <Typography color="gary" className="font-normal text-gray-600">
-                The key to more success is to have a lot of pillows. Put it this way, it took me
-                twenty five years to get these plants, twenty five years o
+                The initial release of CodeForgeAI. The key to more success is to have a lot of
+                pillows. Put it this way, it took me twenty five years to get these plants, twenty
+                five years of blood sweat and tears, and I'm never giving up, I'm just getting
+                started.
               </Typography>
             </TimelineBody>
           </TimelineItem>
@@ -42,27 +44,11 @@ import {
             </TimelineHeader>
             <TimelineBody className="pb-8">
               <Typography color="gary" className="font-normal text-gray-600">
-                The key to more success is to have a lot of pillows. Put it this way, it took me
-                twenty five years to get these plants, twenty five years 
+                Added advanced mode and dark mode. The key to more success.
               </Typography>
             </TimelineBody>
           </TimelineItem>
-          <TimelineItem>
-            <TimelineHeader>
-              <TimelineIcon className="p-2">
-                <CurrencyDollarIcon className="h-4 w-4" />
-              </TimelineIcon>
-              <Typography variant="h5" color="blue-gray">
-                CodeForgeAI V1.0
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody>
-              <Typography color="gary" className="font-normal text-gray-600">
-                The key to more success is to have a lot of pillows. Put it this way, it took me
-                twenty five years to get these plants, twenty five years of 
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
+         
         </Timeline>
       </div>
     );
