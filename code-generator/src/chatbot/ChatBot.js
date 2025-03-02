070ff8160072af505ba524e661cd3105b15d0376
Dhahlan3890 <dhahlan2000@gmail.com>
2025-03-02 14:39:00 +0530

    Fix image preview handling to correctly access nested image data in ChatBot component

--- code-generator/src/chatbot/ChatBot.js
+++ code-generator/src/chatbot/ChatBot.js
@@ -118,7 +118,7 @@ function ChatBot() {
     console.log(`Clicked history item at index: ${index}`);
     console.log('History item:', history[index]);
     setResult(history[index].result);
-    setImagePreview(`data:image/png;base64,${history[index].image}`);
+    setImagePreview(`data:image/png;base64,${history[index].image.image}`);
   };
 
   const handleDeleteHistory = (id) => {
